ba63db26658dadae536c82c17409742d9db238ed
Daniel Corson <danl@alum.mit.edu>
2009-07-06 17:56:18 -0700

    type-sigs in Main.hs

--- src/FriendFast.hs
+++ src/FriendFast.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE DeriveDataTypeable #-}
-module FriendFast where 
+module FriendFast where
 
 import qualified Client.Facebook as C
 import Client.Login
@@ -12,26 +12,27 @@ import qualified Data.ByteString.Internal as BSI
 import qualified Data.ByteString as BS
 
 data FriendFast = FriendFast {
-      name :: String,
-      uid :: C.UID}
-    deriving (Data, Typeable, Show)
+  name :: String,
+  uid :: C.UID}
+  deriving (Data, Typeable, Show)
 
 fetchFriendsFast ::  C.UID -> C.FacebookM [FriendFast]
-fetchFriendsFast uid = do 
-    friends <- C.fql_query $ "SELECT name, uid FROM user where uid in (select uid2 from friend where uid1 = "++ show uid ++ ")"
-    return $ decodeJSON friends
+fetchFriendsFast uid = do
+  friends <- C.fql_query $
+    "SELECT name, uid FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE \
+    \uid1 = " ++ show uid ++ ")"
+  return $ decodeJSON friends
 
 fetchFriendStorage :: C.UID -> C.FacebookM (T.Trie FriendFast)
 fetchFriendStorage f_uid = do
   friends <- fetchFriendsFast f_uid
-  let assoc = concat $ map (\f-> zip (BS.splitWith (== (BSI.c2w ' ')) $ BS.pack $ map (BSI.c2w) $ map toLower $ name f) $ repeat $ f) friends 
+  let
+    assoc = concat $ map (\ f -> zip (BS.splitWith (== (BSI.c2w ' ')) .
+      BS.pack . map (BSI.c2w) . map toLower $ name f) $ repeat f) friends
   return $ T.fromList $ assoc
 
-
 getFriendsWithName :: (T.Trie FriendFast) -> String -> [(String, FriendFast)]
-getFriendsWithName trie name =
-    T.toListBy justNames subtrie
-    where
-      justNames name f = (map BSI.w2c $ BS.unpack $ name, f) 
-      subtrie = T.submap bsName trie 
-      bsName = BS.pack $ map (BSI.c2w) $ map toLower $ name 
+getFriendsWithName trie name = T.toListBy justNames subtrie where
+  justNames name f = (map BSI.w2c $ BS.unpack $ name, f)
+  subtrie = T.submap bsName trie
+  bsName = BS.pack $ map (BSI.c2w) $ map toLower $ name
--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@ import Control.Applicative
 import Control.Arrow
 import Control.Monad
 import Control.Monad.Reader
+import Control.Monad.State
 import Data.Binary
 import Data.Char
 import Data.List
@@ -28,6 +29,8 @@ import qualified Client.Login as Login
 import qualified Events as E
 import qualified FriendFast as FF
 import qualified HSH as HSH
+
+version :: [Integer]
 version = [0, 0]
 
 type Permission = String
@@ -35,15 +38,13 @@ type Uid = Integer
 data Options = Options {
   optHelp :: Bool,
   optVersion :: Bool,
-  optReFetchCached :: Bool
-}
+  optReFetchCached :: Bool}
 
 defOpts :: Options
 defOpts = Options {
   optHelp = False,
   optVersion = False,
-  optReFetchCached = False
-}
+  optReFetchCached = False}
 
 options :: [OptDescr (Options -> Options)]
 options = [
@@ -52,7 +53,7 @@ options = [
   Option "" ["version"] (NoArg (\ o -> o {optVersion = True}))
     "Show version",
   Option "c" ["re-fetch-cached"] (NoArg (\ o -> o {optReFetchCached = True}))
-    "Re-fetch any cached data"
+    "Re-fetch any cached data"]
   {-
   Option "" [""] (ReqArg (\ a o -> o {opt = read a}) "")
     ""
@@ -61,17 +62,17 @@ options = [
   Option "" [""] (NoArg (\ o -> o {opt = }))
     ""
   -}
-  ]
 
-baseConfig = Client.FacebookConfig
-             {Client.apiKey = "80d6cc5ca397c92c9cd41cfe09380b9d",
-              Client.secretKey = "282d51185be4ed0a8c28def8b8688b18",
-              Client.endPoint = "http://api.srush2.devrs006.facebook.com/restserver.php"}
+baseConfig :: Client.FacebookConfig
+baseConfig = Client.FacebookConfig {
+  Client.apiKey = "80d6cc5ca397c92c9cd41cfe09380b9d",
+  Client.secretKey = "282d51185be4ed0a8c28def8b8688b18",
+  Client.endPoint = "http://api.srush2.devrs006.facebook.com/restserver.php"}
 
-
+fb :: Client.FacebookM a -> IO a
 fb = Client.runFacebook baseConfig
 
-
+fbCmds :: [FbCmd]
 fbCmds = [
   fbInit,
   fbShowCmds,
@@ -84,23 +85,22 @@ fbCmds = [
   fbUserStream,
   fbEvent,
   fbStatus,
-  fbWall
-  ]
+  fbWall]
 
 data FbCmd = FbCmd {
   fbCmdName :: String,
   fbCmdHelp :: String,
-  fbCmdFunc :: (Options, [String]) ->  Client.FacebookM ()
-  }
-
+  fbCmdFunc :: (Options, [String]) -> Client.FacebookM ()}
 
 -- maybe this is a silly way to do this.
 -- after all, we could extract the command list from help.
 -- anyway, this was easy.
+fbShowCmds :: FbCmd
 fbShowCmds =
   FbCmd "commands" "Show list of all commands (for tab-completion)" . const .
   io . putStr . unlines $ map fbCmdName fbCmds
 
+ensureDir :: FilePath -> IO ()
 ensureDir dir = unlessM (doesDirectoryExist dir) $ createDirectory dir
 
 day :: NominalDiffTime
@@ -157,25 +157,20 @@ ensureLogin = do
   Just user <- Client.getUser
   return user
 
-
+requestPermission :: String -> Client.FacebookM ()
 requestPermission perm = Login.askPermission perm
 
 ensurePermission :: Permission -> Client.FacebookM ()
 ensurePermission perm = do
   -- todo, deal with opts
-  cache defOpts ("perm1:"++perm) day $ requestPermission perm
+  cache defOpts ("perm1:" ++ perm) day $ requestPermission perm
 
-
+fbInit :: FbCmd
 fbInit = FbCmd "init" "Initialize a facebook account" $ \ (opts,args) -> do
   user <- ensureLogin
   io $ printf "%d \n" user
 
---fbFriends = FbCmd "friends" "Show list of all friends" $ \ (opts, args) -> do
-  --user <- ensureLogin
-  --friends <- getFriends -- cache opts "friends" day
-  -- fix this
-  --io $ print friends
-
+fbFriends :: FbCmd
 fbFriends = FbCmd "friends" "Get your friend list" $ \ (opts, args) -> do
   user <- ensureLogin
   friends <- FF.fetchFriendsFast user
@@ -201,11 +196,14 @@ renderFriend = renderJustsOnX [
   ("Quotations:", quotes),
   ("About:",      about_me)]
 
+renderEvent :: E.Event -> [(String, String)]
 renderEvent = renderJustsOnX [
   ("Name:",        Just . E.name),
   ("Tagline:",     E.tagline),
   ("Description:", E.description)]
 
+nameToUid :: Client.UID -> [Char] -> StateT Client.SessionConfig
+  (ReaderT Client.FacebookConfig IO) (Maybe Client.UID)
 nameToUid user fname =
     if isDigit $ head fname then
         return $ Just $ read fname
@@ -221,7 +219,7 @@ nameToUid user fname =
       io $ mapM_ (\(_,f) -> printf "%d - %s\n"  (FF.uid f) (FF.name f)) $ ls
       return Nothing
 
-
+showHTML :: String -> IO ()
 showHTML rendered = do
   file <- openFile "/tmp/fbtextbook" WriteMode
   hPutStrLn file $ rendered
@@ -229,7 +227,7 @@ showHTML rendered = do
   system "cat /tmp/fbtextbook | w3m -T text/html"
   return ()
 
-
+fbFinger :: FbCmd
 fbFinger = FbCmd "finger" "Get a friend's info" $ \ (opts, [fname]) -> do
   user <- ensureLogin
   uid <- nameToUid user fname
@@ -240,87 +238,100 @@ fbFinger = FbCmd "finger" "Get a friend's info" $ \ (opts, [fname]) -> do
     Nothing ->
         return ()
 
+fbEvent :: FbCmd
 fbEvent = FbCmd "events" "Get all my events" $ \ (opts, _) -> do
   user <- ensureLogin
   events <- E.fetchEvents user
   io $ showHTML $ concat $ map(\e -> (ddize (renderEvent e) ++  "<br>")) events
 
 
+fbProfilePic :: FbCmd
 fbProfilePic = FbCmd "profilepic" "See a profie pic" $ \ (opts, [fname]) -> do
   user <- ensureLogin
   fuid <- nameToUid user fname
   case fuid of
     Just fuid -> do
-             friend <- fetchFriend fuid
-             io $ case pic_big friend of
-                 Just pic -> do {system ("jp2a --colors \"" ++ pic ++ "\"");return ()}
-                 Nothing  -> putStrLn "No pic available"
-    Nothing ->
-        return ()
+      friend <- fetchFriend fuid
+      io $ case pic_big friend of
+        Just pic -> do {system ("jp2a --colors \"" ++ pic ++ "\"");return ()}
+        Nothing  -> putStrLn "No pic available"
+    Nothing -> return ()
 
-
-
-
+fbHi :: FbCmd
 fbHi = FbCmd "hi" "Just for testing/lols" . const $
   io $ print "hi"
 
+fbHome :: FbCmd
 fbHome = FbCmd "home" "Show home page information" . const $
   error "TODO home"
 
+fbProfile :: FbCmd
 fbProfile = FbCmd "poke" "Check pokes or poke someone" . const $
   error "TODO poke"
 
+fbPoke :: FbCmd
 fbPoke = FbCmd "profile" "Show profile page information" . const $
   error "TODO profile"
 
-fbWall = FbCmd "wall" "Write on a friends wall" $ \ (opts, [fname, message]) -> do
-  user <- ensureLogin
-  ensurePermission "publish_stream"
-  uid <- nameToUid user fname
-  case uid of
-    Just uid -> do
-        Client.stream_publish message $ Just uid
-        io $ printf "Wrote \"%s\" on to: %s\n" message uid
-    Nothing -> return ()
+fbWall :: FbCmd
+fbWall = FbCmd "wall" "Write on a friends wall" $
+  \ (opts, [fname, message]) -> do
+    user <- ensureLogin
+    ensurePermission "publish_stream"
+    uid <- nameToUid user fname
+    case uid of
+      Just uid -> do
+          Client.stream_publish message $ Just uid
+          io $ printf "Wrote \"%s\" on to: %s\n" message uid
+      Nothing -> return ()
 
+fbStatus :: FbCmd
 fbStatus = FbCmd "status" "Set your status" $ \ (opts, [status]) -> do
   user <- ensureLogin
   ensurePermission "publish_stream"
   Client.stream_publish status Nothing
   io $ printf "Status set to: %s\n" status
 
+fbStream :: FbCmd
 fbStream = FbCmd "stream" "Get your stream" $ \ (opts, _) -> do
   user <- ensureLogin
   ensurePermission "read_stream"
   (stream, idmap) <- fetchStream user
 
-  io $ showHTML $ ddize $ mapMaybe (format idmap) stream
-    where format idmap (JSObject sobj) = case get_field sobj "message" of
-                                 Just (JSString m) -> Just $ ((idmap ! (sobj !^ "actor_id")), (fromJSString m))
-                                 _ -> Nothing
-          format  _ _= Nothing
+  io $ showHTML $ ddize $ mapMaybe (format idmap) stream where
+    format idmap (JSObject sobj) =
+      case get_field sobj "message" of
+        Just (JSString m) ->
+          Just $ ((idmap ! (sobj !^ "actor_id")), (fromJSString m))
+        _ -> Nothing
+    format  _ _= Nothing
 
+fbUserStream :: FbCmd
+fbUserStream = FbCmd "userstream" "Get a users stream" $
+  \ (opts, [fname]) -> do
+    user <- ensureLogin
+    ensurePermission "read_stream"
+    uid <- nameToUid user fname
+    case uid of
+      Just uid -> do
+          (stream, idmap) <- fetchUserStream uid
+          io $ showHTML $ ddize $ mapMaybe (format idmap) stream
+      Nothing -> return ()
+    where
+      format idmap (JSObject sobj) =
+        case get_field sobj "message" of
+          Just (JSString m) ->
+            Just $ ((idmap ! (sobj !^ "actor_id")), (fromJSString m))
+          _ -> Nothing
+      format  _ _= Nothing
 
-fbUserStream = FbCmd "userstream" "Get a users stream" $ \ (opts, [fname]) -> do
-  user <- ensureLogin
-  ensurePermission "read_stream"
-  uid <- nameToUid user fname
-  case uid of
-    Just uid -> do
-        (stream, idmap) <- fetchUserStream uid
-        io $ showHTML $ ddize $ mapMaybe (format idmap) stream
-    Nothing -> return ()
-   where format idmap (JSObject sobj) = case get_field sobj "message" of
-                                               Just (JSString m) -> Just $ ((idmap ! (sobj !^ "actor_id")), (fromJSString m))
-                                               _ -> Nothing
-         format  _ _= Nothing
-
-
-fbBirthday = FbCmd "birthday" "Get birthdays for a given day" $ \ (opts, [date]) -> do
-  user <- ensureLogin
-  friends <- fetchFriends user
-  let friends' = filter (maybe False (isPrefixOf date) . birthday) friends
-  io $ mapM_ (printf "Birthday: %s\n" . show .birthday) friends'
+fbBirthday :: FbCmd
+fbBirthday = FbCmd "birthday" "Get birthdays for a given day" $
+  \ (opts, [date]) -> do
+    user <- ensureLogin
+    friends <- fetchFriends user
+    let friends' = filter (maybe False (isPrefixOf date) . birthday) friends
+    io $ mapM_ (printf "Birthday: %s\n" . show .birthday) friends'
 
 
 commandList :: String
